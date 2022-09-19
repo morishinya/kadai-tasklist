fefe76d58c63a22f4b32f342bc1cec9392423e4c
shinya.mori <shinya.mori@axas-japan.co.jp>
2022-09-19 23:50:19 +0900

    Lesson16_Chapter17_課題

---
+++ src/controllers/IndexServlet.java
@@ -0,0 +1,67 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Tasklist;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+        List<Tasklist> tasklists = em.createNamedQuery("getAllTasklists", Tasklist.class)
+                                   .setFirstResult(15 * (page - 1))
+                                   .setMaxResults(15)
+                                   .getResultList();
+
+        // 全件数を取得
+        long tasklists_count = (long)em.createNamedQuery("getTasklistsCount", Long.class)
+                                      .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("tasklists", tasklists);
+        request.setAttribute("tasklists_count", tasklists_count);     // 全件数
+        request.setAttribute("page", page);
+
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklists/index.jsp");
+        rd.forward(request, response);
+    }
+
+}
